adbe8ba423331a0e69ab3b7f45b29b5e7f6ceafb
Joao Lucas <lucasmonteirolima17@gmail.com>
2024-05-12 15:12:05 +0000

    changes in adder lib

---
+++ 02/Adder/adder.cpp
@@ -0,0 +1,9 @@
+#include "adder.h"
+
+int adder::add(int a, int b){
+    return (a+b);
+}
+
+float adder::add(float a, float b){
+    return (a+b);
+}
---
+++ 02/Adder/adder.h
@@ -0,0 +1,4 @@
+namespace adder {
+    int add(int a, int b);
+    float add(float a, float b);
+}
